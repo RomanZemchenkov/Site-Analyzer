95bd70258c7b3c3184e81b70e30d4722a0b683d1
Roman Zemchenkov <unknownuzer@yandex.ru>
2024-10-09 00:36:06 +0300

    Delete extra method from IndexService

--- src/main/java/searchengine/services/service/IndexService.java
+++ src/main/java/searchengine/services/service/IndexService.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import searchengine.dao.model.Index;
-import searchengine.dao.model.Lemma;
 import searchengine.dao.repository.index.IndexRepository;
-import searchengine.dao.repository.lemma.LemmaRepository;
 
 import java.util.List;
 
@@ -16,22 +14,9 @@ import java.util.List;
 public class IndexService {
 
     private final IndexRepository indexRepository;
-    private final LemmaRepository lemmaRepository;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void createIndex(List<Index> indexesBySite){
-        indexRepository.batchSave(parseToIndex(indexesBySite));
-    }
-
-    private List<Index> parseToIndex(List<Index> indexList){
-        for(Index index : indexList){
-            Lemma indexLemma = index.getLemma();
-            if(indexLemma.getId() == null){
-                indexLemma = lemmaRepository.findLemmaByLemmaAndSite(indexLemma.getLemma(), indexLemma.getSite());
-                System.out.println("В другой раз повезёт больше");
-                index.setLemma(indexLemma);
-            }
-        }
-        return indexList;
+        indexRepository.batchSave(indexesBySite);
     }
 }
