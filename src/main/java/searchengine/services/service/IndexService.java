71e7b28d28efaba485651043d56db75f3098fb38
Roman Zemchenkov <unknownuzer@yandex.ru>
2024-10-09 00:09:24 +0300

    Delete method from IndexService

--- src/main/java/searchengine/services/service/IndexService.java
+++ src/main/java/searchengine/services/service/IndexService.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import searchengine.dao.model.Index;
-import searchengine.dao.model.Lemma;
 import searchengine.dao.repository.index.IndexRepository;
-import searchengine.dao.repository.lemma.LemmaRepository;
 
 import java.util.List;
 
@@ -15,23 +13,11 @@ import java.util.List;
 public class IndexService {
 
     private final IndexRepository indexRepository;
-    private final LemmaRepository lemmaRepository;
 
     @Transactional()
     public void createIndex(List<Index> indexesBySite){
         System.out.println("Начало сохранения индексов");
-        indexRepository.batchSave(parseToIndex(indexesBySite));
+        indexRepository.batchSave(indexesBySite);
     }
 
-    private List<Index> parseToIndex(List<Index> indexList){
-        for(Index index : indexList){
-            Lemma indexLemma = index.getLemma();
-            if(indexLemma.getId() == null){
-                indexLemma = lemmaRepository.findLemmaByLemmaAndSite(indexLemma.getLemma(), indexLemma.getSite());
-                System.out.println("В другой раз повезёт больше");
-                index.setLemma(indexLemma);
-            }
-        }
-        return indexList;
-    }
 }
